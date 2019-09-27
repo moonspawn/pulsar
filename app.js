30198e944eccb5ced716116e0ef21d05ee1784ec
Anup Joseph <anup20joseph@gmail.com>
2019-09-27 19:37:30 +0530

    New login page

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.use(session({secret:'dfgdfgrfv',saveUninitialized: true,resave: false}))
 
